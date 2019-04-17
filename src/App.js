4c363f43b2c3c32c89096952edc6816f9e22358c
TomaszMoroz <tmoroz688@gmail.com>
2019-04-17 19:08:50 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <h2>No to zaczynamy Janek :)</h2>
       </div>
     );
